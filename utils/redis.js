47023f9cd2a70b7c0d67af4834498ed821cb2b8b
Abuka-Victor <abukavictoro@gmail.com>
2023-03-07 01:23:09 +0100

    Promisify methods and add main.js file for testing

--- utils/redis.js
+++ utils/redis.js
@@ -1,29 +1,41 @@
-const { createClient } = require('redis');
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
+// class to define methods for commonly used redis commands
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.log('Redis Client Error', err));
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
   }
 
+  // check connection status and report
   isAlive() {
-    if (this.client.isReady) return true;
+    if (this.client.connected) return true;
     return false;
   }
 
+  // get value for given key from redis server
   async get(key) {
-    return this.client.get(key);
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
   }
 
-  async set(key, value, duration) {
-    await this.client.setex(key, duration, value);
+  // set key value pair to redis server
+  async set(key, value, time) {
+    const redisSetex = promisify(this.client.setex).bind(this.client);
+    await redisSetex(key, time, value);
   }
 
+  // del key vale pair from redis server
   async del(key) {
-    await this.client.del(key);
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
   }
 }
 
-const redisClient = RedisClient();
+const redisClient = new RedisClient();
 
 module.exports = redisClient;
